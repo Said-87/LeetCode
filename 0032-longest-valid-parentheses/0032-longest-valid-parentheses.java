79900ccecfdeacb4f4450e5c940b23cdb22f789e
Said Ul Amin <saidulamin062@gmail.com>
2026-08-22 10:21:22 +0500

    Time: 5 ms (80.72%), Space: 46.6 MB (43.88%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -0,0 +1,25 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class Solution {
+    public int longestValidParentheses(String s) {
+        int maxLength = 0;
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(-1); // Base boundary
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(i); // New base boundary
+                } else {
+                    maxLength = Math.max(maxLength, i - stack.peek());
+                }
+            }
+        }
+
+        return maxLength;
+    }
+}
