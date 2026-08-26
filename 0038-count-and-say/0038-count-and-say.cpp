1352b8b66bd06b164772f1c39e93f3f00551a533
Said Ul Amin <saidulamin062@gmail.com>
2026-08-26 19:52:44 +0500

    Time: 3 ms (81.2%), Space: 9.8 MB (62.64%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        string result = "1";
+
+        for (int i = 2; i <= n; i++) {
+            string next = "";
+
+            int count = 1;
+
+            for (int j = 1; j <= result.length(); j++) {
+                if (j < result.length() && result[j] == result[j - 1]) {
+                    count++;
+                } else {
+                    next += to_string(count);
+                    next += result[j - 1];
+                    count = 1;
+                }
+            }
+
+            result = next;
+        }
+
+        return result;
+    }
+};
