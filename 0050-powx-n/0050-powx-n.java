f32987f1ac7c191cb0faa51975b389dddb0f2b57
Said Ul Amin <saidulamin062@gmail.com>
2026-08-29 22:02:45 +0500

    Time: 0 ms (100%), Space: 46.8 MB (98.46%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,24 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long power = n;  // use long to handle Integer.MIN_VALUE
+
+        if (power < 0) {
+            x = 1 / x;
+            power = -power;
+        }
+
+        double result = 1.0;
+
+        while (power > 0) {
+            // If power is odd
+            if (power % 2 == 1) {
+                result *= x;
+            }
+
+            x *= x;
+            power /= 2;
+        }
+
+        return result;
+    }
+}
