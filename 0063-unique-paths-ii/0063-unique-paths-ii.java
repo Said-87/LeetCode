201615ef00b25a046b0ae93be37ed4e089699bb0
Said Ul Amin <saidulamin062@gmail.com>
2026-09-02 19:30:12 +0500

    Time: 0 ms (100%), Space: 43.7 MB (27.12%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+
+        int[] dp = new int[n];
+
+        // Starting point
+        dp[0] = 1;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                // Obstacle means there are 0 ways to reach this cell
+                if (obstacleGrid[i][j] == 1) {
+                    dp[j] = 0;
+                } 
+                else if (j > 0) {
+                    dp[j] = dp[j] + dp[j - 1];
+                }
+            }
+        }
+
+        return dp[n - 1];
+    }
+}
